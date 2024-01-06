6ddde830b85e8fa3e73ee692a298beeec822ad69
Beknazar <85113284+beknazar93@users.noreply.github.com>
2024-01-07 00:00:10 +0600

    Add files via upload

---
+++ backend/api/product.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/", async (req,res) => {
+    try{
+        res.json({
+            status: 200,
+            message:'server work'
+        })
+
+    }catch(error){
+        console.error(error)
+        return res.status(500).send('Server error')
+    }
+})
+
+module.exports = router;
